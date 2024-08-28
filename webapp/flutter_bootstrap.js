8f64a3f56aa844c1f504802f59efbcf327527e64
Erfan Rahmati <ErfanRht2005@gmail.com>
2024-08-29 01:32:10 +0330

    web app configuration update

--- webapp/flutter_bootstrap.js
+++ webapp/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba9960f","builds":[{"compileTarget":"dart2js","renderer":"skwasm","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba9960f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1716436315"
+    serviceWorkerVersion: "4080425334"
   }
 });
